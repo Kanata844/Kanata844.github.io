24d2d72bce91bed4c85df576fc2c719c13f227e4
Kanata844 <ta3wata@gmail.com>
2025-12-13 17:38:05 +0900

    とりあえず静的ファイル出力

---
+++ src/app/page1/page.tsx
@@ -0,0 +1,9 @@
+export default function Hoge() {
+  return(
+    <>
+      <h1>これは別のページだよ</h1>
+      <p><a href="/">元のページに戻る</a></p>
+      <p><a href="/page1/page2">さらに別のページに行く</a></p>
+    </>
+  );
+}
---
+++ src/app/page1/page2/page.tsx
@@ -0,0 +1,8 @@
+export default function Hoge() {
+  return(
+    <>
+      <h1>これはさらに別のページだよ</h1>
+      <p><a href="/">元のページに戻る</a></p>
+    </>
+  );
+}
