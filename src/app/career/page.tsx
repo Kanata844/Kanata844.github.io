04b7ec1f44bc0d828d9f1cd404c2a4101f9dd444
Kanata844 <ta3wata@gmail.com>
2025-12-15 04:10:07 +0900

    画像のサイズ調整

--- src/app/career/page.tsx
+++ src/app/career/page.tsx
@@ -2,6 +2,7 @@
 
 import Header from "@/components/header/Header"
 import Image from "next/image";
+import styles from "./page.module.css"
 import tatekan from "../../../public/images/tatekan.jpg"
 
 export default function career() {
@@ -9,8 +10,8 @@ export default function career() {
     <>
     <Header/>
     <h1>経歴</h1>
-    <p>準備中でっせちょっとまってくんなまし、駒場のよくわからない大学でボコボコにされてるよ</p>
-    <Image src={tatekan} alt="5人に1人留年"></Image>
+    <p>駒場のよくわからない大学でボコボコにされてるよ</p>
+    <Image className={styles.tatekan} src={tatekan} alt="5人に1人留年" width={768} height={1024}></Image>
     </>
   );
 }
