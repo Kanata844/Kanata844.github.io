252253141bc7cb1677100e7a68a85901940f85c7
Kanata844 <ta3wata@gmail.com>
2025-12-14 21:53:50 +0900

    css modulesを使うように変更

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,101 +1,30 @@
 'use client'
 
 import {useState} from 'react';
+import styles from "./Header.module.css"
 
 export default function Header(){
     let [menuClicked, setMenuClicked] = useState(false);
     return (
     <>
-    <header>
-        <div className="top">
+    <header className={styles.header}>
+        <div className={styles.top}>
             <div><a href="/">カナタのホームページ</a></div>
-            <p className="mobile-only" onClick={()=>{setMenuClicked(!menuClicked)}}>{menuClicked? "✕": "三"}</p>
+            <p className={styles.mobile_only} onClick={()=>{setMenuClicked(!menuClicked)}}>{menuClicked? "✕": "三"}</p>
         </div>
-        <div className="nav-pc pc-only">
+        <div className={`${styles.nav_pc} ${styles.pc_only}`}>
             <div><a href="/works">作品</a></div>
             <div><a href="/career">経歴</a></div>
             <div><a href="/about">このサイトについて</a></div>
             <div><a href="/links">各種リンク</a></div>
         </div>
     </header>
-    <div className="nav-mobile mobile-only" style={{right: menuClicked? "0": "-200px"}}>
+    <div className={`${styles.nav_mobile} ${styles.mobile_only}`} style={{right: menuClicked? "0": "-200px"}}>
         <p><a href="/works">作品</a></p>
         <p><a href="/career">経歴</a></p>
         <p><a href="/about">このサイトについて</a></p>
         <p><a href="/links">各種リンク</a></p>
     </div>
-    <style jsx>{`
-    .mobile-only{
-        display: none;
-    }
-    header {
-        width: 100%;
-        height: auto;
-        background-color: black;
-        position: sticky;
-    }
-    .top {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-    .top div{
-        margin: auto;
-        margin-top: 10px;
-        margin-bottom: 20px;
-    }
-    .top a{
-        color: white;
-        text-decoration: none;
-        font-size: 4vw;
-        font-family: serif;
-        font-weight: bold;
-    }
-    .top p{
-        color: white;
-        margin-right: 5px;
-        width: 35px;
-        font-size: 30px;
-    }
-    .nav-pc{
-        display: flex;
-        justify-content: center;
-    }
-    .nav-pc div{
-        display: flex;
-        justify-content: center;
-        width: 150px;
-        margin: auto;
-    }
-    .nav-mobile{
-        position: fixed;
-        background-color: gray;
-        width: 200px;
-        height: 100%;
-        transition: 0.5s;
-    }
-    .nav-mobile p{
-        margin: 5px;
-    }
-    .nav-mobile a{
-        color: white;
-        text-decoration: none;
-    }
-    .nav-pc a{
-        color: white;
-        text-decoration: none;
-        margin: auto;
-        margin-bottom: 5px;
-    }
-    @media screen and (max-width: 767px){
-        .pc-only {
-            display: none;
-        }
-        .mobile-only{
-            display: block;
-        }
-    }
-    `}</style>
     </>);
 } 
     
