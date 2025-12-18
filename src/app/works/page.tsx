016598e6a043a3b068265fc5ce88e27be9966f7f
Kanata844 <ta3wata@gmail.com>
2025-12-18 23:09:41 +0900

    執筆中追加

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -1,13 +1,14 @@
 'use client'
 
 import Header from "@/components/header/Header"
+import Link from "next/link"
 
 export default function works(){
     return (
     <>
     <Header/>
     <h1>作品</h1>
-    <p>準備中 ちょっとまってね</p>
+    <p>絶賛執筆中:<Link href="https://kanata844.github.io/gh-pages-tutorial">こいつ</Link></p>
     </>
     )
 }
